f37cce80ff18bd1dd48419dcc1c4fdfa7e2916e6
gurunath9201 <gurunathmule825@gmail.com>
2026-03-14 14:48:29 +0530

    Second Commit

--- GitHubDemo.java
+++ GitHubDemo.java
@@ -2,6 +2,9 @@ package com.fullstack.github;
 
 public class GitHubDemo {
 	public static void main(String[] args) {
+		int a=10,b=20;
+		int c=a+b;
 		System.out.println("Github Demo");
+		System.out.println("Addition is:"+c);
 	}
 }
